72f2453b07c28716cf5d07765ac25a68567b45e4
Ange-H0610 <hei.iraka.4@gmail.com>
2025-07-30 18:17:08 +0300

    feat: add navbar, theme switch button, and improved image card design

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,17 @@
 export default function PokemonCard({ name, image }) {
-    return (
-      <div className="bg-white dark:bg-gray-800 p-4 rounded shadow text-center hover:scale-105 transition">
-        <img src={image} alt={name} className="mx-auto w-20 h-20" />
-        <h2 className="mt-2 font-semibold capitalize text-white">{name}</h2>
+  return (
+    <div className="relative bg-gradient-to-tr from-blue-200 via-white to-purple-100 border-[3px] border-purple-400 rounded-3xl shadow-xl p-4 transform transition duration-500 hover:rotate-[-1deg] hover:scale-105 hover:shadow-purple-400 hover:-translate-y-2 cursor-pointer">
+      <div className="bg-white rounded-2xl p-4 shadow-inner border border-purple-200 hover:border-purple-300 transition duration-300">
+        <img
+          src={image}
+          alt={name}
+          className="w-28 h-28 mx-auto mb-4 drop-shadow-lg"
+        />
+        <h2 className="capitalize text-xl font-bold text-purple-800 drop-shadow-sm text-center">
+          {name}
+        </h2>
       </div>
-    );
-  }
-  
+      <div className="absolute inset-0 rounded-3xl border-4 border-transparent hover:border-purple-500 pointer-events-none transition-all duration-500" />
+    </div>
+  );
+}
