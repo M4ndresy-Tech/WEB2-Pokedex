185b46ebbaad2be2ea3e03a329bbde850b9dadaf
Soa <hei.soa.3@gmail.com>
2025-07-31 04:56:20 +1300

    jut de quelques améliorations de design

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ export default function App() {
     const [selectedType, setSelectedType] = useState("all");
     const [typeMap, setTypeMap] = useState({});
 
+<<<<<<< HEAD
     useEffect(() => {
         fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
             .then((res) => res.json())
@@ -25,6 +26,54 @@ export default function App() {
                                 [poke.name]: details.types.map((t) => t.type.name),
                             }));
                         });
+=======
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then((res) => res.json())
+      .then((data) => setPokemons(data.results));
+  }, []);
+
+  const filtered = pokemons.filter((poke) =>
+    poke.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <div className="min-h-screen bg-white text-gray-900">
+      <h1 className="text-4xl font-bold text-center text-red-600 py-4">
+  Pokédex National
+</h1>
+
+      {/* Barre du haut : champ de recherche */}
+      <div className="flex justify-center items-center p-4 max-w-4xl mx-auto">
+        <div className="relative w-full">
+          <input
+            type="text"
+            placeholder="Rechercher un Pokémon..."
+            className="w-full pl-10 pr-4 py-2 rounded border"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <i className="fas fa-search absolute left-3 top-1/2 -translate-y-1/2 text-gray-500"></i>
+        </div>
+      </div>
+
+      
+      <div className="grid grid-cols-2 sm:grid-cols-4 gap-4 p-4 max-w-4xl mx-auto">
+        {filtered.map((poke) => {
+          const id = poke.url.split("/")[6];
+          const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+
+          const handleClick = () => {
+            fetch(`https://pokeapi.co/api/v2/pokemon/${poke.name}`)
+              .then((res) => res.json())
+              .then((data) => {
+                setSelectedPokemon({
+                  name: data.name,
+                  image: image,
+                  weight: data.weight,edr,
+                  height: data.height,
+                  types: data.types,
+>>>>>>> d7c1373 (Ajout de quelques améliorations de design)
                 });
             });
 
